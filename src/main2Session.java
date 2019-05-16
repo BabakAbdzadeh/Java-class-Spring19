35f7cfeec637d8e390d28a14d6377612a998a358
BabakAbdzadeh <Babak.abdzadeh@gmail.com>
2019-05-16 10:46:52 +0430

    First work Edit 1

--- src/main2Session.java
+++ src/main2Session.java
@@ -5,6 +5,35 @@ public class main2Session {
         System.out.print("enter your age please:");
         Scanner age_variable = new Scanner(System.in);
         int age = age_variable.nextInt();
-        System.out.println("Your age is:"+age);
+        if (age > 15 && age < 24){
+            System.out.println("you are young");
+        }
+        else if (age > 24 && age <50 ){
+            System.out.println("you are middle age");
+        }
+        else if (age == 24){
+            System.out.println("you are exactly 24");
+        }
+        else {
+            System.out.println("we cant tell about you");
+        }
     }
 }
+// int age ; => this is declaration
+// age = 18 ; this is initialize
+// deference between print and println ( we use println for times we want to /n our caret after print)
+
+
+// Conditional loops :
+// <<<if>>> example:
+// int age = 18
+// if ( age > 15 && age < 24 ) {
+//    Scope
+//    }
+// else if(age>24 && age <50) {
+//     scope
+// }
+// else {
+// scope
+//  {
+//
